8093705302a152385b672f9ee7df6ada09dbc94b
YOGESH256 <2019yogesh.khatri@ves.ac.in>
2021-08-07 22:55:26 +0530

    day four build completed

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ module.exports = {
   images :{
     domains:['links.papareact.com'],
   },
+  env:{
+    mapbox_key :'pk.eyJ1IjoieW9nZXNoa2hhdHJpIiwiYSI6ImNrczFwb3VpZzF3MngycHBzdnVyY2JneGoifQ.5r167zpS6Z-kK7Y-xobAGA'
+  }
 
 
 }
